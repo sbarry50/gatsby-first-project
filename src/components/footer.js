eae40e7b5ee9349b91106eda4944b8fdbcecb3c9
Steve <sbarry50@gmail.com>
2019-02-19 11:20:07 -0500

    First commit

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./footer.module.css"
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        <h2>Built by Steve, my first Gatsby site!</h2>
+    </footer>
+)
+
+export default Footer
---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = ({data}) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi folks. How are you doing?</h1>
+    <p>{data.site.siteMetadata.description}</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
+export const query = graphql`
+{
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
